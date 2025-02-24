20d9046f17c31c524fb5e8b659d77df8fa61807b
User <User@DESKTOP-KK11SB1>
2025-02-24 19:45:21 +0530

    Add class Added

---
+++ DemoWebShop/src/test/java/GitHub/AddClass.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddClass {
+@Test
+public void Demo() {
+	Reporter.log("Add class Added", true);
+}
+}
